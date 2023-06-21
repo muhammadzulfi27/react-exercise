61a69a6bb994bbbcd33db97e7156572396a05e5c
muhammadzulfi27 <ramadhanizulfi28@gmail.com>
2023-06-21 10:10:20 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = 'YOURDEPLOYURL'; // TODO: replace this
+const studentName = 'Muhammad Zulfi'; // TODO: replace this
+const studentId = 'FE5'; // TODO: replace this
+
+export { url, studentName, studentId };
